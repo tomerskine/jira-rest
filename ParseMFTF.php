52b0b40da0f2b206d130d123d74dcc988c0a4d68
terskine <erskine.tom@gmail.com>
2018-07-12 20:19:26 -0500

    f

--- ParseMFTF.php
+++ ParseMFTF.php
@@ -8,10 +8,16 @@ use Magento\FunctionalTestingFramework\Test\Handlers\TestObjectHandler;
 //class ParseMFTF {
 
     function getTestObjects() {
-        $testObjects = TestObjectHandler::getInstance()->getAllObjects();
+        //$testObjects = TestObjectHandler::getInstance()->getAllObjects();
+        $testObjects = file_get_contents(__DIR__ . '/samplejson/testObjects.json');
+        //print_r($json);
+        //testObjects = json_decode($json);
+        //print_r('hello');
+        print_r($testObjects[0]);
+        print_r($testObjects[2]);
         return $testObjects;
     }
 //}
 
 $testObjects = getTestObjects();
-var_dump($testObjects);
+//var_dump($testObjects);
