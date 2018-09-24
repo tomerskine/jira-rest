fec65b017ef02732c1d1536f5b81e502b8057106
Tom Erskine <erskine.tom@gmail.com>
2018-09-23 23:16:09 -0500

    UpdateIssue skipped test transition and issue link

--- UpdateIssue.php
+++ UpdateIssue.php
@@ -13,13 +13,16 @@ require 'vendor/autoload.php';
 use JiraRestApi\Issue\IssueService;
 use JiraRestApi\Issue\IssueField;
 use JiraRestApi\JiraException;
+use JiraRestApi\Issue\Transition;
+use JiraRestApi\IssueLink\IssueLink;
+use JiraRestApi\IssueLink\IssueLinkService;
 
 class UpdateIssue {
 
-    public $toUpdate;
+    public $Update;
 
-    function __construct($toUpdate){
-        $this->toUpdate = $toUpdate;
+    function __construct($Update){
+        $this->toUpdate = $Update;
     }
 
     function updateIssueREST(){
@@ -61,8 +64,15 @@ class UpdateIssue {
     }
 
     // TODO : REMOVE
-    static function updateDryRunIssuesREST($test)
+    static function updateDryRunIssuesREST($update)
     {
+        $issueField = self::buildUpdateIssueField($update);
+        LoggingUtil::getInstance()->getLogger(UpdateIssue::class)->info('TEST sent to UPDATE : ' . key($update);
+        // TODO : Add call to REAL update issue REST call
+        if (isset($update['skip'])) {
+            self::skipTestStatusTransition($update);
+            self::skipTestLinkIssue($update);
+        }
         //need to make multiple payloads for mass create issue
         //need each test as separate array of tuple
         // TODO : MOVE TO FUNCTION
@@ -70,7 +80,7 @@ class UpdateIssue {
         $severity = (isset($test['severity'])) ? $test['severity'] : '2-major';
         $group = 'DEFAULT GROUP VALUE'; // TODO : NEED TO AGREE ON DEFAULT VALUE - POSSIBLY CATALOG ?
 
-        $issueField = new IssueField();
+
 
         $issueField->setProjectKey('TOMERSKINE')//TODO: No project in MFTF array
         ->setSummary($test['title'][0])// Use Title from MFTF array
@@ -90,5 +100,55 @@ class UpdateIssue {
             ->addCustomField('customfield_12720', ['value' => $severity])// TODO: for any customfields taking LIST, need to ['value' => 'foo'] and [ ['value => 'foo'], ['value' => 'bar'] ] or multiple list selections
         ;
         return $issueField;
+    }
+
+    public static function buildUpdateIssueField($update) {
+        $issueField = new IssueField();
+        if (isset($update['title'])) {
+            $issueField->setSummary($update['title']);
+        }
+        if (isset($update['description'])) {
+            $issueField->setDescription($update['description']);
+        }
+        if (isset($update['stories'])) {
+            $issueField->addCustomField('customfield_14364', $update['stories']);
+        }
+        if (isset($update['severity'])) {
+            $issueField->addCustomField('customfield_12720', ['value' => $update['severity']]);
+        }
+        return $issueField;
+    }
+
+    public function skipTestStatusTransition($update) {
+        $issueKey = key($update);
+        try {
+            $transition = new Transition();
+            $transition->setTransitionName('Skipped');
+            $transition->setCommentBody('MFTF INTEGRATION - Setting SKIPPED status.');
+
+            $skipTransitionIssueService = new IssueService();
+
+            $skipTransitionIssueService->transition($issueKey, $transition);
+        } catch (JiraException $e) {
+            $this->assertTrue(FALSE, "add Comment Failed : " . $e->getMessage());
+        }
+    }
+
+    public function skipTestLinkIssue($update) {
+        try {
+            $il = new IssueLink();
+
+            $il->setInwardIssue(key($update))
+                ->setOutwardIssue($update['skip'])
+                ->setLinkTypeName('Blocks' )
+                ->setComment('Blocking issue for Skipped test');
+
+            $ils = new IssueLinkService();
+
+            $ret = $ils->addIssueLink($il);
+
+        } catch (JiraException $e) {
+            print("Error Occured! " . $e->getMessage());
+        }
     }
 }
